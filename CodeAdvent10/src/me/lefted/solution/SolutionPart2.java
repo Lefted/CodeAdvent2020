a94d643b8ae9b612b30789ac069c3d74b66b8c56
Lefted <gartnermoritz@yahoo.de>
2020-12-16 19:05:59 +0100

    Organize imports

--- CodeAdvent10/src/me/lefted/solution/SolutionPart2.java
+++ CodeAdvent10/src/me/lefted/solution/SolutionPart2.java
@@ -3,7 +3,6 @@ package me.lefted.solution;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
